3ff17d8a7322082e6c8a60005b079bbd28c1a73d
heeycoen <coen.kenter@me.com>
2023-07-10 13:40:54 +0200

    mask fix

--- Metrics/sauvola_masking.py
+++ Metrics/sauvola_masking.py
@@ -3,8 +3,15 @@ from scipy.ndimage.morphology import binary_fill_holes
 from skimage.filters.thresholding import threshold_sauvola
 from skimage.morphology.binary import binary_dilation
 
-from Preprocessing.Preprocessor import denoise_in
+import cv2
 import numpy as np
+
+
+def denoise_in(input, denoise):
+    if denoise:
+        return cv2.fastNlMeansDenoising(input.astype('uint8'), None, 10, 7, 21)
+    else:
+        return input
 
 
 def sauvola_mask(array, multiplier=255):
