df50b3d3b77544fb778d99d67f92f947d0b2288d
heeycoen <coen.kenter@me.com>
2023-07-11 13:33:44 +0200

    scoring with mse and rmse

--- Metrics/Scoring.py
+++ Metrics/Scoring.py
@@ -7,37 +7,37 @@ from sklearn.metrics import mean_squared_error
 import yaml
 from yaml.loader import SafeLoader
 import argparse
-from sewar.full_ref import mse
+from sewar.full_ref import mse, rmse
 def compare_models(root):
     z = [x.split("/") for x in
          glob(root + "/*.h5", recursive=True)]
     z = [x[len(x) - 1] for x in z]
     z = [x[0:len(x) - 3] for x in z]
     print(z)
-    print("File, VMSE, PMSE, VSSIM_masked,VDice_masked,PSSIM_masked,PDice_masked,VMSSIM_masked,VMDice_masked,PMSSIM_masked,PMDice_masked,VCSSIM_masked,VCDice_masked,PCSSIM_masked,PCDice_masked,VMSSIM,PMSSIM,VCSSIM,PCSSIM,VSSIM,PSSIM")
-
+    print("File,VMSE,PMSE,VRMSE,PRMSE")
     for file in z:
         result = h5py.File(f"{root}/{file}.h5", 'r')
         PixPrediction = np.array([result["pix2pix"][1], result["pix2pix"][0]]) / 255
         VoxPrediction = np.array(result["prediction"])
         Truth = np.array(result["truth"])
 
-        VMSSIM_masked, VMDice_masked, PMSSIM_masked, PMDice_masked = ssim_scoring.Masked_SSIM_SingleChan(VoxPrediction[0], PixPrediction[0], Truth[0])
-        VCSSIM_masked, VCDice_masked, PCSSIM_masked, PCDice_masked = ssim_scoring.Masked_SSIM_SingleChan(VoxPrediction[1], PixPrediction[1], Truth[1])
-        VMSSIM = ssim_scoring.ssim(VoxPrediction[0], Truth[0], win_size=7, channel_axis=0)
-        PMSSIM = ssim_scoring.ssim(PixPrediction[0], Truth[0], win_size=7, channel_axis=0)
-        VCSSIM = ssim_scoring.ssim(VoxPrediction[1], Truth[1], win_size=7, channel_axis=0)
-        PCSSIM = ssim_scoring.ssim(PixPrediction[1], Truth[1], win_size=7, channel_axis=0)
-        VSSIM = ssim_scoring.ssim(VoxPrediction, Truth, win_size=7, channel_axis=0)
-        PSSIM = ssim_scoring.ssim(PixPrediction, Truth, win_size=7, channel_axis=0)
-
-        VSSIM_masked, VDice_masked,_,_ = ssim_scoring.Masked_SSIM_AVG(VoxPrediction, Truth)
-        PSSIM_masked, PDice_masked,_,_ = ssim_scoring.Masked_SSIM_AVG(PixPrediction, Truth)
+            # VMSSIM_masked, VMDice_masked, PMSSIM_masked, PMDice_masked = ssim_scoring.Masked_SSIM_SingleChan(VoxPrediction[0], PixPrediction[0], Truth[0])
+            # VCSSIM_masked, VCDice_masked, PCSSIM_masked, PCDice_masked = ssim_scoring.Masked_SSIM_SingleChan(VoxPrediction[1], PixPrediction[1], Truth[1])
+            # VMSSIM = ssim_scoring.ssim(VoxPrediction[0], Truth[0], win_size=7, channel_axis=0)
+            # PMSSIM = ssim_scoring.ssim(PixPrediction[0], Truth[0], win_size=7, channel_axis=0)
+            # VCSSIM = ssim_scoring.ssim(VoxPrediction[1], Truth[1], win_size=7, channel_axis=0)
+            # PCSSIM = ssim_scoring.ssim(PixPrediction[1], Truth[1], win_size=7, channel_axis=0)
+            # VSSIM = ssim_scoring.ssim(VoxPrediction, Truth, win_size=7, channel_axis=0)
+            # PSSIM = ssim_scoring.ssim(PixPrediction, Truth, win_size=7, channel_axis=0)
+            #
+            # VSSIM_masked, VDice_masked,_,_ = ssim_scoring.Masked_SSIM_AVG(VoxPrediction, Truth)
+            # PSSIM_masked, PDice_masked,_,_ = ssim_scoring.Masked_SSIM_AVG(PixPrediction, Truth)
 
         PMSE = mse(Truth, PixPrediction)
         VMSE = mse(Truth, VoxPrediction)
-        print(f"{file},{VMSE},{PMSE},{VSSIM_masked},{VDice_masked},{PSSIM_masked},{VCDice_masked},{VMSSIM_masked},{VMDice_masked},{PMSSIM_masked},{PMDice_masked},{VCSSIM_masked},{VCDice_masked},{PCSSIM_masked},{PCDice_masked},{VMSSIM},{PMSSIM},{VCSSIM},{PCSSIM},{VSSIM},{PSSIM}")
-
+        PRMSE = rmse(Truth, PixPrediction)
+        VRMSE = rmse(Truth, VoxPrediction)
+        print(f"{file},{VMSE},{PMSE},{VRMSE},{PRMSE}")
 
 
 
