Scoring rotate masking fix

Metrics/sauvola_masking.py
@@ -18,7 +18,7 @@ def sauvola_mask(array, multiplier=255):
     blurred_array = array * multiplier
     blurred_array = blurred_array.astype(int)
     blurred_array_2 = []
-    for i in range(128):
+    for i in range(blurred_array.shape[0]):
         blurred_array_2.append(denoise_in(blurred_array[i], True))
     blurred_array_2 = np.array(blurred_array_2)
     blurred_array_2 = gaussian_filter(blurred_array_2, sigma=2)
@@ -30,7 +30,7 @@ def sauvola_mask(array, multiplier=255):
     for i in range(4):
         mask_array = binary_dilation(mask_array)
         mask_array = binary_fill_holes(mask_array)
-        for i in range(128):
+        for i in range(mask_array.shape[0]):
             mask_array[i] = binary_fill_holes(mask_array[i])
 
     return mask_array