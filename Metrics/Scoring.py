cf338d2aaee7477ab56a276aacf4271916680cc0
heeycoen <coen.kenter@me.com>
2023-07-18 12:23:41 +0200

    Scoring function

--- Metrics/Scoring.py
+++ Metrics/Scoring.py
@@ -33,23 +33,15 @@ def compare_models(root,name,dataset_name):
         RMSE = rmse(Truth, Prediction)
         print(f"{file},{name},{MSE},{RMSE},{SSIM_normal}")
 
-def SSIM_result(PixPrediction, VoxPrediction, Truth):
-    VMSSIM_masked, VMDice_masked, PMSSIM_masked, PMDice_masked = ssim_scoring.Masked_SSIM_SingleChan(VoxPrediction[0],
-                                                                                                     PixPrediction[0],
-                                                                                                     Truth[0])
-    VCSSIM_masked, VCDice_masked, PCSSIM_masked, PCDice_masked = ssim_scoring.Masked_SSIM_SingleChan(VoxPrediction[1],
-                                                                                                     PixPrediction[1],
-                                                                                                     Truth[1])
-    VMSSIM = ssim_scoring.ssim(VoxPrediction[0], Truth[0], win_size=7, channel_axis=0)
-    PMSSIM = ssim_scoring.ssim(PixPrediction[0], Truth[0], win_size=7, channel_axis=0)
-    VCSSIM = ssim_scoring.ssim(VoxPrediction[1], Truth[1], win_size=7, channel_axis=0)
-    PCSSIM = ssim_scoring.ssim(PixPrediction[1], Truth[1], win_size=7, channel_axis=0)
-    VSSIM = ssim_scoring.ssim(VoxPrediction, Truth, win_size=7, channel_axis=0)
-    PSSIM = ssim_scoring.ssim(PixPrediction, Truth, win_size=7, channel_axis=0)
+def SSIM_result(Prediction, Truth):
+    VMSSIM_masked, VMDice_masked ,_,_= ssim_scoring.Masked_SSIM(Prediction[0], Truth[0])
+    VCSSIM_masked, VCDice_masked,_,_= ssim_scoring.Masked_SSIM(Prediction[1], Truth[1])
+    VMSSIM = ssim_scoring.ssim(Prediction[0], Truth[0], win_size=7, channel_axis=0)
+    VCSSIM = ssim_scoring.ssim(Prediction[1], Truth[1], win_size=7, channel_axis=0)
+    VSSIM = ssim_scoring.ssim(Prediction, Truth, win_size=7, channel_axis=0)
 
-    VSSIM_masked, VDice_masked, _, _ = ssim_scoring.Masked_SSIM_AVG(VoxPrediction, Truth)
-    PSSIM_masked, PDice_masked, _, _ = ssim_scoring.Masked_SSIM_AVG(PixPrediction, Truth)
-    return f"{VSSIM_masked},{VDice_masked},{PSSIM_masked},{VCDice_masked},{VMSSIM_masked},{VMDice_masked},{PMSSIM_masked},{PMDice_masked},{VCSSIM_masked},{VCDice_masked},{PCSSIM_masked},{PCDice_masked},{VMSSIM},{PMSSIM},{VCSSIM},{PCSSIM},{VSSIM},{PSSIM}"
+    VSSIM_masked, VDice_masked, _, _ = ssim_scoring.Masked_SSIM_AVG(Prediction, Truth)
+    return f"{VSSIM_masked},{VDice_masked},{VMSSIM_masked},{VMDice_masked},{VCSSIM_masked},{VCDice_masked},{VMSSIM},{VCSSIM},{VSSIM}"
 
 
 if __name__ == '__main__':
