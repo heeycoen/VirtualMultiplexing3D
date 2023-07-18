460f19cf6ce676e22d3951e79cbcc3b73db0cd10
heeycoen <coen.kenter@me.com>
2023-07-18 12:13:49 +0200

    Scoring function

--- Metrics/Scoring.py
+++ Metrics/Scoring.py
@@ -8,36 +8,30 @@ import yaml
 from yaml.loader import SafeLoader
 import argparse
 from sewar.full_ref import mse, rmse
-def compare_models(root):
+def compare_models(root,name,dataset_name):
     z = [x.split("/") for x in
          glob(root + "/*.h5", recursive=True)]
     z = [x[len(x) - 1] for x in z]
     z = [x[0:len(x) - 3] for x in z]
-    ssim_keys = ['VSSIM_masked','VDice_masked','PSSIM_masked','PDice_masked','VMSSIM_masked','VMDice_masked','PMSSIM_masked','PMDice_masked','VNSSIM_masked','VNDice_masked','PNSSIM_masked','PNDice_masked','VMSSIM,PMSSIM','VNSSIM,PCSSIM','VSSIM,PSSIM']
-    printstring = "File,VMSE,PMSE,VRMSE,PRMSE"
+    ssim_keys = ['SSIM_masked','Dice_masked','MSSIM_masked','MDice_masked','NSSIM_masked','NDice_masked','MSSIM','NSSIM','SSIM']
+    printstring = f"File,model,MSE,RMSE"
     for key in ssim_keys:
         printstring = f"{printstring},{key}"
-    for key in ssim_keys:
-        printstring = f"{printstring},{key}_XY"
-    for key in ssim_keys:
-        printstring = f"{printstring},{key}_ZY"
     print(printstring)
 
     for file in z:
         result = h5py.File(f"{root}/{file}.h5", 'r')
-        PixPrediction = np.array([result["pix2pix"][1], result["pix2pix"][0]]) / 255
-        VoxPrediction = np.array(result["prediction"])
+        if dataset_name == "pix2pix":
+            Prediction = np.array([result["pix2pix"][1], result["pix2pix"][0]]) / 255
+        else:
+            Prediction = np.array(result[dataset_name])
         Truth = np.array(result["truth"])
 
-        SSIM_normal = SSIM_result(PixPrediction,VoxPrediction,Truth)
-        SSIM_Rot_XY = SSIM_result(np.rot90(PixPrediction,k=1,axes=(1,2)),np.rot90(VoxPrediction,k=1,axes=(1,2)),np.rot90(Truth,k=1,axes=(1,2)))
-        SSIM_Rot_ZY = SSIM_result(np.rot90(PixPrediction,k=1,axes=(2,3)),np.rot90(VoxPrediction,k=1,axes=(2,3)),np.rot90(Truth,k=1,axes=(2,3)))
+        SSIM_normal = SSIM_result(Prediction,Truth)
 
-        PMSE = mse(Truth, PixPrediction)
-        VMSE = mse(Truth, VoxPrediction)
-        PRMSE = rmse(Truth, PixPrediction)
-        VRMSE = rmse(Truth, VoxPrediction)
-        print(f"{file},{VMSE},{PMSE},{VRMSE},{PRMSE},{SSIM_normal},{SSIM_Rot_XY},{SSIM_Rot_ZY}")
+        MSE = mse(Truth, Prediction)
+        RMSE = rmse(Truth, Prediction)
+        print(f"{file},{name},{MSE},{RMSE},{SSIM_normal}")
 
 def SSIM_result(PixPrediction, VoxPrediction, Truth):
     VMSSIM_masked, VMDice_masked, PMSSIM_masked, PMDice_masked = ssim_scoring.Masked_SSIM_SingleChan(VoxPrediction[0],
@@ -69,4 +63,4 @@ if __name__ == '__main__':
     with open(args.c) as f:
         data = yaml.load(f, Loader=SafeLoader)
 
-    compare_models(data["input"])
+    compare_models(data["input"],data["name"],data["dataset_name"])
